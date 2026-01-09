f7f654c481af60e03c8d11463d8cf34727b3e0c9
risma pradnya <rismapradnya26@gmail.com>
2026-01-09 18:00:43 +0800

    Update database.php

--- database.php
+++ database.php
@@ -2,7 +2,7 @@
 $DB_HOST = 'localhost';
 $DB_USER = 'root';
 $DB_PASS = ''; // kosongkan kalau phpMyAdmin tidak minta login
-$DB_NAME = 'todo_app'; // ganti sesuai database kamu
+$DB_NAME = 'todo_app'; // ganti sesuai database yang anda buat
 
 $conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 
